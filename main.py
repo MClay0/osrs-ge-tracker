e5a21dab56d6e97ecda7f250587f646ceddfe4d8
Mason Clay <mason_clay@outlook.com>
2024-10-28 01:37:24 -0400

    Set up all API enpoints, Implemented as methods

---
+++ main.py
@@ -0,0 +1,15 @@
+from ge_page_endpoints import *
+from osrs_wiki_endpoints import *             
+
+if __name__ == "__main__":
+    #print(getItemDetails(2))
+    #print(getCategoryDetails("a",1))
+    #print(getGraphData(2))
+    #print(getLatestPrice())
+    #print(getLatestPrice(2))   
+    #print(getWikiMap())   
+    #print(getFiveMinPrices())
+    #print(getFiveMinPrices(1615733400))  
+    #print(getOneHourPrices())
+    #print(getOneHourPrices(1615734000))
+    print(getTimeseries(2,"6h"))
